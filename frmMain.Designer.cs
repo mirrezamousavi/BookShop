e9ce39c7e87e6198a52fcf5ab9013ee4c2cc8e2d
mirrezamousavi <mousavi199005@gmail.com>
2024-10-02 09:13:28 +0330

    Add project files.

---
+++ frmMain.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace BookShop
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mnuiBaseInfo = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuCategory = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuBook = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuCustomer = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuiAction = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuOrder = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuiHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuCalculator = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuiBaseInfo,
+            this.mnuiAction,
+            this.mnuiHelp});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.menuStrip1.Size = new System.Drawing.Size(800, 29);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "mnuMain";
+            // 
+            // mnuiBaseInfo
+            // 
+            this.mnuiBaseInfo.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuCategory,
+            this.mnuBook,
+            this.mnuCustomer});
+            this.mnuiBaseInfo.Name = "mnuiBaseInfo";
+            this.mnuiBaseInfo.Size = new System.Drawing.Size(112, 25);
+            this.mnuiBaseInfo.Text = "ثبت اطلاعات";
+            // 
+            // mnuCategory
+            // 
+            this.mnuCategory.Name = "mnuCategory";
+            this.mnuCategory.Size = new System.Drawing.Size(175, 26);
+            this.mnuCategory.Text = "دسته بندی";
+            // 
+            // mnuBook
+            // 
+            this.mnuBook.Name = "mnuBook";
+            this.mnuBook.Size = new System.Drawing.Size(175, 26);
+            this.mnuBook.Text = "کتاب";
+            // 
+            // mnuCustomer
+            // 
+            this.mnuCustomer.Name = "mnuCustomer";
+            this.mnuCustomer.Size = new System.Drawing.Size(175, 26);
+            this.mnuCustomer.Text = "مشتری";
+            // 
+            // mnuiAction
+            // 
+            this.mnuiAction.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuOrder});
+            this.mnuiAction.Name = "mnuiAction";
+            this.mnuiAction.Size = new System.Drawing.Size(75, 25);
+            this.mnuiAction.Text = "عملیات";
+            // 
+            // mnuOrder
+            // 
+            this.mnuOrder.Name = "mnuOrder";
+            this.mnuOrder.Size = new System.Drawing.Size(152, 26);
+            this.mnuOrder.Text = "سفارش";
+            // 
+            // mnuiHelp
+            // 
+            this.mnuiHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuAbout,
+            this.mnuCalculator,
+            this.mnuExit});
+            this.mnuiHelp.Name = "mnuiHelp";
+            this.mnuiHelp.Size = new System.Drawing.Size(66, 25);
+            this.mnuiHelp.Text = "راهنما";
+            // 
+            // mnuAbout
+            // 
+            this.mnuAbout.Name = "mnuAbout";
+            this.mnuAbout.Size = new System.Drawing.Size(197, 26);
+            this.mnuAbout.Text = "درباه ";
+            // 
+            // mnuCalculator
+            // 
+            this.mnuCalculator.Name = "mnuCalculator";
+            this.mnuCalculator.Size = new System.Drawing.Size(197, 26);
+            this.mnuCalculator.Text = "ماشین حساب";
+            // 
+            // mnuExit
+            // 
+            this.mnuExit.Name = "mnuExit";
+            this.mnuExit.Size = new System.Drawing.Size(197, 26);
+            this.mnuExit.Text = "خروج";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "فروشگاه کتاب";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuiBaseInfo;
+        private System.Windows.Forms.ToolStripMenuItem mnuCategory;
+        private System.Windows.Forms.ToolStripMenuItem mnuBook;
+        private System.Windows.Forms.ToolStripMenuItem mnuCustomer;
+        private System.Windows.Forms.ToolStripMenuItem mnuiAction;
+        private System.Windows.Forms.ToolStripMenuItem mnuOrder;
+        private System.Windows.Forms.ToolStripMenuItem mnuiHelp;
+        private System.Windows.Forms.ToolStripMenuItem mnuAbout;
+        private System.Windows.Forms.ToolStripMenuItem mnuCalculator;
+        private System.Windows.Forms.ToolStripMenuItem mnuExit;
+    }
+}
+
